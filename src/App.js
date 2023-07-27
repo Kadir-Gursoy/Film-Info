38ad627dfc70a371dd256584c53db9499829d9f9
Mcclayn798 <mnkmcc9@gmail.com>
2023-07-27 15:26:00 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { Routes, Route } from "react-router-dom"
+import Main from "./pages/main"
+function App() {
+  return (
+    <div className="App">
+      
+<Routes>
+    <Route path="/" element={<Main/>}>
+    <Route>
+    <Route>
+    <Route>
+
+
+
+
+
+
+</Routes>
+
+    </div>
+  )
+}
+
+export default App
